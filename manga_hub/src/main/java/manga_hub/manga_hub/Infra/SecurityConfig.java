a2c49e1c8925f266f30f4b78d844c3eed74b45d3
Rafael Correa <140332334+Coder-Rafael@users.noreply.github.com>
2024-07-16 00:50:42 -0300

    Update SecurityConfig.java

--- manga_hub/src/main/java/manga_hub/manga_hub/Infra/SecurityConfig.java
+++ manga_hub/src/main/java/manga_hub/manga_hub/Infra/SecurityConfig.java
@@ -13,10 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import manga_hub.manga_hub.Security.SecurityFilter;
 
 
@@ -26,7 +22,7 @@ public class SecurityConfig {
     
    @Autowired
    SecurityFilter securityFilter;
-    
+   
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
@@ -43,21 +39,6 @@ public class SecurityConfig {
         )   .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();  
     } 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("https://mangahub.up.railway.app");
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
